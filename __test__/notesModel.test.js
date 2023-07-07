d5056e400f4b8a6cde859c4d713d6b044e3d159d
Ehijie <eaghedo1@gmail.com>
2023-07-07 12:50:58 +0100

    test implemented getNotes function

--- __test__/notesModel.test.js
+++ __test__/notesModel.test.js
@@ -4,4 +4,9 @@ describe('NotesModel class', () => {
     it('initially constructs', () => {
         const model = new NotesModel();
     });
+
+    it('initially returns empty list of notes using getNotes()', () => {
+        const model = new NotesModel();
+        expect(model.getNotes()).toEqual([]);
+    });
 })
--- src/notesModel.js
+++ src/notesModel.js
@@ -1,5 +1,12 @@
 class NotesModel {
+
+    constructor() {
+        this.notes = [];
+    }
     
+    getNotes() {
+        return this.notes;
+    }  
 }
 
 module.exports = NotesModel;
