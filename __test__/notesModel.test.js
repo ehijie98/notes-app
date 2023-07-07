a9af629100058e2a2459bc39006a085be0427d49
Ehijie <eaghedo1@gmail.com>
2023-07-07 12:01:50 +0100

    NotesModel initially constructs test passed

---
+++ __test__/notesModel.test.js
@@ -0,0 +1,7 @@
+const NotesModel = require('./../src/notesModel.js');
+
+describe('NotesModel class', () => {
+    it('initially constructs', () => {
+        const model = new NotesModel();
+    });
+})
