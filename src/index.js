242672ad266ca554f10b507d1570b4f8e3762782
Ehijie <eaghedo1@gmail.com>
2023-08-25 14:59:25 +0100

    removed addNote function calling inside index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const NotesView = require("./notesView");
 
 console.log("The notes app is running");
 const model = new NotesModel();
-model.addNote('Hopefully we see this');
 console.log(model.getNotes());
 const view = new NotesView(model);
 view.displayNotes()
