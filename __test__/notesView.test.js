85d3698a72f9de6ac228cf12ada3fef4e33875b2
Ehijie <eaghedo1@gmail.com>
2023-08-25 14:53:56 +0100

    test added to display correct number of notes and stop duplicates

--- __test__/notesView.test.js
+++ __test__/notesView.test.js
@@ -33,4 +33,22 @@ describe('NotesView class', () => {
         expect(document.querySelectorAll('div.note').length).toEqual(1);
         expect(document.querySelectorAll('div.note')[0].textContent).toEqual("Test note")
     })
+
+    it('displays correct number of notes', () => {
+        const model = new NotesModel();
+        const view = new NotesView(model);
+        const input = document.querySelector('#note-input');
+        input.value = "First note";
+
+        const button = document.querySelector("#add-note-button");
+        button.click();
+
+        const secondInput = document.querySelector('#note-input');
+        secondInput.value = "Second note";
+        button.click();
+
+        expect(document.querySelectorAll('div.note').length).toEqual(2);
+
+
+    })
 })
